23286764fdf2846f8cbac7dc3d3efe975ec8133f
sakronos <yangcheng1999@foxmail.com>
2020-06-05 23:22:19 +0800

    6/5 Bell Rings backup 2

--- app/src/main/java/com/example/alarmclock/BellRingsActivity.java
+++ app/src/main/java/com/example/alarmclock/BellRingsActivity.java
@@ -3,11 +3,18 @@ package com.example.alarmclock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.IOException;
+
 public class BellRingsActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,18 @@ public class BellRingsActivity extends AppCompatActivity {
         TextView time = findViewById(R.id.AlarmBell_time);
         Intent intent = getIntent();
         time.setText(intent.getStringExtra("time"));
+        mediaPlayer = MediaPlayer.create(this,Uri.fromFile(new File("/system/media/audio/alarms/Platinum.ogg")));
+        mediaPlayer.setLooping(true);
+
+
+
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.start();
+                }
+            });
+
 
         findViewById(R.id.stop_bell).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,5 +42,15 @@ public class BellRingsActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mediaPlayer!=null){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        super.onDestroy();
     }
 }
